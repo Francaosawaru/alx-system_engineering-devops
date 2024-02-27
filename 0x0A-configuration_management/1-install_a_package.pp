810f796d9e606f46d00ecf9c94f469c9795bc1e6
Francaosawaru <forierie@yahoo.com>
2024-02-27 03:11:29 -0800

    A-configuration_management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,15 @@
-# A puppet manifest installing flask v2.1.0 which is a package from pip3.
-# Am also specifying the version of flask to install using the ensure attribute
+# install flask from pip3
+
 package { 'flask':
   ensure   => '2.1.0',
-  provider => pip3,
+  provider => 'pip3',
 }
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
+
+package { 'Pythong':
+  ensure   => '3.8.10',
+  provider => 'pip3',
