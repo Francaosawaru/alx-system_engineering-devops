31232d7547abf2d5e0ceb826ff2c3f6c0d3a2195
Francaosawaru <forierie@yahoo.com>
2024-02-27 11:43:26 -0800

    task completed

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -2,9 +2,9 @@
 # The file (School) has permissions 0744, belonging to the user and group
 # www-data.
 file { '/tmp/school':
-  ensure  => present,
+  ensure  => file,
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet',
+  content => "I love Puppet\n",
 }
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,7 @@
-# Kills a process named killmenow
+# This manifest kills a specific process using the execute command
 
-exec { 'pkill -f killmenow':
-  path => '/usr/bin/:/usr/local/bin/:/bin/'
+exec { 'kill_killmenow_process':
+  command     => '/usr/bin/pkill killmenow',
+  path        => ['/usr/bin', '/bin'],
+  refreshonly => true,
 }
